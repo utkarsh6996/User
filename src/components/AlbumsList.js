29fc216162dcd327810c6b243d462368f87aee26
utkarsh6996 <utkarsh@highbreed.co>
2023-05-09 14:11:23 +0530

    first commit

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -2,7 +2,7 @@
 
 import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
 import Skeleton from './Skeleton';
-import ExpandablePanel from './ExpandablePanel';
+//import ExpandablePanel from './ExpandablePanel';
 import Button from './Button';
 import AlbumsListItem from './AlbumsListItem';
 
